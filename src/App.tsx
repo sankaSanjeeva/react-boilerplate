3ea002c97afb65ddd99b76541823bc2872bf42d8
Sanka <sankasanjeevadg@gmail.com>
2024-07-07 23:20:50 +0530

    set initial route to 'home'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { AuthProvider, ThemeProvider } from './common/contexts';
+import { Header } from './common/components';
+
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-svh">
-      <p className="font-semibold text-4xl text-sky-500">React Boilerplate</p>
-    </div>
+    <ThemeProvider>
+      <AuthProvider>
+        <Header />
+
+        {/* height of Outlet = screen size - (height of header + bottom border width) */}
+        <div className="max-w-screen-xl mx-auto min-h-[calc(100svh_-_50px)] flex">
+          <Outlet />
+        </div>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 

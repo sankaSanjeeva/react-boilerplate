00ad4adfc111067b5c1131973b5da50d333207a8
Sanka <sankasanjeevadg@gmail.com>
2024-09-07 17:09:42 +0530

    ♻️  Removed useless commented lines

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
-export const PAGE_SIZE = 10;
-/**
- * Make sure PAGE_SIZE is inside the PAGE_SIZES
- */
 export const PAGE_SIZES = [5, 10, 20, 30, 40, 50];
+export const PAGE_SIZE = PAGE_SIZES[1];
